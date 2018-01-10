e1cd16fd47f414a0f85d9b218cb29695a906aabd
Alex Mikhailik <alex-under-2010@mail.ru>
2018-01-11 00:57:18 +0200

    Initial commit

---
+++ Key Esc/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace Key_Esc
+{
+    sealed partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 7000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button1
+            // 
+            this.button1.AutoSize = true;
+            this.button1.BackColor = System.Drawing.Color.LightGreen;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(167, 34);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Включить";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.AutoSize = true;
+            this.button2.BackColor = System.Drawing.Color.LightGreen;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(12, 52);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(167, 34);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Выключить";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonFace;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.ClientSize = new System.Drawing.Size(191, 99);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.KeyPreview = true;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Opacity = 0.9D;
+            this.Text = "Anti Globus";
+            this.TopMost = true;
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        partial void TimerTick();
+
+        public System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ Key Esc/Form1.cs
@@ -0,0 +1,72 @@
+﻿#region
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Drawing;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Windows.Forms;
+using Microsoft.Win32;
+
+#endregion
+
+namespace Key_Esc
+{
+    public sealed partial class Form1 : Form
+    {
+        
+        [DllImport("User32.dll")]
+        static extern void mouse_event(MouseFlags dwFlags, int dx, int dy, int dwData, UIntPtr dwExtraInfo);
+
+        [Flags]
+        enum MouseFlags
+        {
+            Move = 0x0001, LeftDown = 0x0002, LeftUp = 0x0004, RightDown = 0x0008,
+            RightUp = 0x0010, Absolute = 0x8000
+        };
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            const int x = 64700;
+            const int y = 1900;
+            mouse_event(MouseFlags.Absolute | MouseFlags.Move, x, y, 0, UIntPtr.Zero);
+            mouse_event(MouseFlags.Absolute | MouseFlags.LeftDown, x, y, 0, UIntPtr.Zero);
+            System.Threading.Thread.Sleep(100);
+            mouse_event(MouseFlags.Absolute | MouseFlags.LeftUp, x, y, 0, UIntPtr.Zero);
+            System.Threading.Thread.Sleep(500);
+            const int x1 = 32768;
+            const int y1 = 33190;
+            mouse_event(MouseFlags.Absolute | MouseFlags.Move, x1, y1, 0, UIntPtr.Zero);
+            mouse_event(MouseFlags.Absolute | MouseFlags.LeftDown, x1, y1, 0, UIntPtr.Zero);
+            System.Threading.Thread.Sleep(100);
+            mouse_event(MouseFlags.Absolute | MouseFlags.LeftUp, x1, y1, 0, UIntPtr.Zero);
+            System.Threading.Thread.Sleep(100);
+            SendKeys.Send("{ESC}");
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private void button1_Click(object sender, EventArgs e)
+        {
+            button1.BackColor = Color.LightGreen;
+            button2.BackColor = Color.LightGreen;
+            timer1.Enabled = true;
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        private void button2_Click(object sender, EventArgs e)
+        {
+            button1.BackColor = Color.LightCoral;
+            button2.BackColor = Color.LightCoral;
+            timer1.Enabled = false;
+        }
+    }
+}
